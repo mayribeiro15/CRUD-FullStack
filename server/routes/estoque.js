031e57e26514ac781857090afeb185eb8a569b81
mayribeiro15 <47997138+mayribeiro15@users.noreply.github.com>
2021-05-10 19:42:09 -0300

    The end

--- server/routes/estoque.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import { getEstoques, createEstoque } from '../controllers/estoque.js'
-const router = express.Router();
-
-router.get('/', getEstoques);
-router.post('/', createEstoque);
-
-export default router;
